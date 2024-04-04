be31d0b61e665b1c678b5a7dc62fec77e7fb632e
Alessandro-Roncaglia <164899374+Alessandro-Roncaglia@users.noreply.github.com>
2024-04-04 16:41:19 +0200

    Update create_table_utenti.sql

--- create_table_utenti.sql
+++ create_table_utenti.sql
@@ -1,1 +1,18 @@
-ciao
+CREATE TABLE Persons (
+    id            INTEGER,
+    first_name    TEXT    NOT NULL,
+    last_name     TEXT    NOT NULL,
+    age           INTEGER,
+    department_id INTEGER,
+    FOREIGN KEY (
+        department_id
+    )
+    REFERENCES Departments,
+    PRIMARY KEY (
+        id
+    ),
+    UNIQUE (
+        first_name,
+        last_name
+    )
+);
