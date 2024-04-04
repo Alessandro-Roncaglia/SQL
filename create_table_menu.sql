449880c5d7b5291d013e05b7dbd7c54bf0a96367
eyaghazouani <165912107+eyaghazouani@users.noreply.github.com>
2024-04-04 17:14:33 +0200

    Update create_table_menu.sql

--- create_table_menu.sql
+++ create_table_menu.sql
@@ -1,9 +1,13 @@
-REATE TABLE menù(
+CREATE TABLE menù(
+id INTEGER
   id_menù text,
   num_menù    TEXT    NOT NULL,
-  id_ristorante INTEGER,
-    PRIMARY KEY (
+  ristorante_id INTEGER,
+  FOREIGN KEY (
+        ristorante_id
+    )
+   REFERENCES ristorante
+   PRIMARY KEY (
         id_menù
     )
 );
-
